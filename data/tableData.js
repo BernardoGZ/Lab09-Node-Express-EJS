087f2b059a86df7485966e6c9fe5e92469a6eff5
BernardoG <A00570682@itesm.mx>
2021-05-06 13:16:25 -0500

    Initial commit

---
+++ data/tableData.js
@@ -0,0 +1,16 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// ===============================================================================
+
+var tableArray = [
+    {
+      customerName: "Bernardo",
+      customerEmail: "bernardo@gmail.com",
+      customerID: "123",
+      phoneNumber: "1111111"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
---
+++ data/waitinglistData.js
@@ -0,0 +1,15 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the waitlist tables.
+// ===============================================================================
+
+var waitingArray = [
+    {
+      
+    }
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = waitingArray;
+  
