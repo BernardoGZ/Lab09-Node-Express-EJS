a0715f31ee85cc0c40a64b850756271571306587
BernardoG <A00570682@itesm.mx>
2021-05-06 17:32:47 -0500

    Update, working on data management

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,14 @@
 // ===============================================================================
 const path = require("path");
 
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var tableData = require("../data/tableData");
+var waitListData = require("../data/waitinglistData");
 
 // ===============================================================================
 // ROUTING
@@ -15,15 +23,18 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+  //Variable if clear lists is needed or not. 
   let quer, clear ;
   // const nom = false;
   
+  //Tables ya no requiere jQuery
   app.get("/tables", function(req, res) {
-    quer = true;
+    quer = false;
     clear = true;
-    res.render('pages/tables', {quer, clear});
+    res.render('pages/tables', {quer, clear, tableData, waitListData});
   });
 
+  //Reservaciones todavia requiere jquery para mandar informacion
   app.get("/reserve", function(req, res) {
     quer = true;
     clear = true;
