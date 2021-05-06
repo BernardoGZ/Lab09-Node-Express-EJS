e923b6f8bd5a211aaca6e6ccfdfed24e62022d76
BernardoG <A00570682@itesm.mx>
2021-05-06 17:11:55 -0500

    Update, ejs views. Pending data transfers

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -15,16 +15,25 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
+  let quer, clear ;
+  // const nom = false;
+  
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    quer = true;
+    clear = true;
+    res.render('pages/tables', {quer, clear});
   });
 
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+    quer = true;
+    clear = true;
+    res.render('pages/reserve', {quer, clear});
   });
 
-  // If no matching route is found default to home
+  // If no matching route is found default to home. Using EJS now
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    quer = false;
+    clear = false;
+    res.render('pages/home', {quer, clear});
   });
 };
